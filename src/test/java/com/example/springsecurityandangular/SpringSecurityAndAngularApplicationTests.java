347dc6ea779ded75b98c283dbacb06d5918a6e3f
Daniel Newton <danielnewt@gmail.com>
2022-11-09 13:14:44 -0500

    Initialized spring project

---
+++ src/test/java/com/example/springsecurityandangular/SpringSecurityAndAngularApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springsecurityandangular;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityAndAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
